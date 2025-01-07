0ccfe7eb974e683c336a5a2440f2729a80a25981
Abdul Rehman Khalid <abdulrehmankhalid906@gmail.com>
2025-01-07 12:06:57 +0500

    dbconfgi

--- dbconfig.php
+++ dbconfig.php
@@ -1,5 +1,10 @@
 <?php
 
+$servername = 'localhost';
+$username = 'gradavrd_school_sy';
+$password = 'gradavrd_expert';
+$dbname = '@jvdK3%yQ*U0';
+
 $con = mysqli_connect($servername,$username,$password,$dbname);
 
 if(!$con)
